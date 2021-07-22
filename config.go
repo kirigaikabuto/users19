34c6597670b7954c51d1717899a39bcdd5b5cbfe
Yerassyl <tleugazy98@gmail.com>
2021-07-22 17:28:00 +0600

    sdsd

---
+++ config.go
@@ -0,0 +1,8 @@
+package users
+
+type MongoConfig struct {
+	Host           string
+	Port           string
+	Database       string
+	CollectionName string
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/kirigaikabuto/users19
+
+go 1.13
+
+require (
+	github.com/google/uuid v1.3.0
+	go.mongodb.org/mongo-driver v1.7.0
+)
